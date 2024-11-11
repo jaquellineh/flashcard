b8b098c942ed3136e6e0b57523081c08553e9cc4
jaquellineh <164918839+jaquellineh@users.noreply.github.com>
2024-11-11 14:15:04 -0300

    cria

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que JavaScript?',
+    'O javaScript é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é París.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
