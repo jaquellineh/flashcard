90c7e8fadf120b602ea125e2f650d6a7c5166021
jaquellineh <164918839+jaquellineh@users.noreply.github.com>
2024-11-18 14:24:04 -0300

    add

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,9 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa.'
 )
+
+criaCartao(
+    'Lígua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é HI.'
+)
